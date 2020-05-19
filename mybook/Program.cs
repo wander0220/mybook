0c5166510e2456896af349aba6bf9acb3fa0acba
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-19 12:51:06 +0900

    list클래스에 값 넣고 출력하기

--- mybook/Program.cs
+++ mybook/Program.cs
@@ -23,6 +23,25 @@ namespace mybook
 
         static void Main(string[] args)
         {
+            List<Book> bookList = new List<Book>();
+            bookList.Add(
+                new Book()
+                {
+                    name="PHP 프로그래밍 입문",
+                    firstPrint = "2013년 5월 20일",
+                    writer="황재호",
+                    pubPerson="김태헌",
+                    publisher="한빛아카데미(주)",
+                    mainEditor="김현용",
+                    planner="김이화",
+                    editor="김이화",
+                    designer="여동일"
+                }
+            );
+            foreach (var item in bookList) 
+            {
+                Console.WriteLine(item.name + " ("+item.writer+") " + item.publisher);
+            }
         }
     }
 }
